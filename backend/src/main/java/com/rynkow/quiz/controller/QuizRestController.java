a867329210fb946b89613eaaa7f27237d0aecf5d
rynkow <rynkowski.sz@gmail.com>
2022-05-04 20:35:00 +0200

    updated quiz controller

--- backend/src/main/java/com/rynkow/quiz/controller/QuizRestController.java
+++ backend/src/main/java/com/rynkow/quiz/controller/QuizRestController.java
@@ -1,79 +1,120 @@
 package com.rynkow.quiz.controller;
 
+import com.rynkow.quiz.dto.DtoMapper;
 import com.rynkow.quiz.dto.QuizDTO;
-import com.rynkow.quiz.model.quiz.Quiz;
+import com.rynkow.quiz.model.question.QuestionStatistics;
+import com.rynkow.quiz.model.quiz.QuizStatistics;
 import com.rynkow.quiz.service.QuizService;
 import com.rynkow.quiz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/quiz")
 public class QuizRestController {
     private QuizService quizService;
     private UserService userService;
+    private DtoMapper dtoMapper;
 
     @GetMapping("/list")
-    public List<QuizDTO> getQuizList() {
+    public List<QuizDTO> getQuizList(Authentication authentication) {
+        final String userName = authentication == null
+                ? null
+                : authentication.getName();
         return quizService.findAll().stream()
-                .filter(Quiz::getPublic)
-                .map(this::convertToDTO)
+                .map(dtoMapper::mapToDTO)
+                .filter(quizDTO -> quizDTO.getPublic() || Objects.equals(userName, quizDTO.getAuthor()))
+                .map(quizDTO -> dtoMapper.fillUserStatistics(quizDTO, userName))
                 .toList();
     }
 
-    @GetMapping("/{quizId}")
-    public QuizDTO getQuizById(@PathVariable String quizId) {
-        return convertToDTO(
-                quizService.findByID(quizId)
-                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "quiz not found"))
-        );
+    @GetMapping("/details/{quizId}")
+    public QuizDTO getQuizById(@PathVariable String quizId, Authentication authentication) {
+        QuizDTO quiz = quizService.findByID(quizId)
+                .map(dtoMapper::mapToDTO)
+                .map(quizDTO -> dtoMapper.fillUserStatistics(quizDTO, authentication.getName()))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "quiz not found"));
+
+        if (!quiz.getPublic() && !Objects.equals(quiz.getAuthor(), authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "unauthorized");
+        return quiz;
     }
 
-    @GetMapping("/{authorId}")
-    public List<QuizDTO> getQuizByAuthorId(@PathVariable String authorId) {
+    @GetMapping("/author/{authorName}")
+    public List<QuizDTO> getQuizByAuthorId(@PathVariable String authorName, Authentication authentication) {
+        String authorId = userService.getId(authorName).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return quizService.findByAuthorId(authorId).stream()
-                .filter(Quiz::getPublic)
-                .map(this::convertToDTO)
+                .filter(quiz -> quiz.getPublic() || Objects.equals(quiz.getAuthorId(), authorId))
+                .map(dtoMapper::mapToDTO)
+                .map(quizDTO -> dtoMapper.fillUserStatistics(quizDTO, authentication.getName()))
                 .toList();
     }
 
     @PostMapping("/create")
-    public ResponseEntity<?> createOrUpdateQuiz(@RequestBody QuizDTO quizDTO) {
-        quizService.saveOrUpdate(convertFromDTO(quizDTO));
-        return new ResponseEntity<>("quiz saved successfully", HttpStatus.OK);
+    public ResponseEntity<?> createQuiz(@RequestBody QuizDTO quizDTO, Authentication authentication) {
+        if (!Objects.equals(quizDTO.getAuthor(), authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        if (quizDTO.getId() != null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        quizService.saveOrUpdate(dtoMapper.mapFromDTO(quizDTO));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> UpdateQuiz(@RequestBody QuizDTO quizDTO, Authentication authentication) {
+        if (!Objects.equals(quizDTO.getAuthor(), authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        String authorName = quizService.getQuizAuthorName(quizDTO.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        if (!Objects.equals(authorName, authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        quizService.saveOrUpdate(dtoMapper.mapFromDTO(quizDTO));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PostMapping("/delete/{id}")
-    public ResponseEntity<?> deleteQuiz(@PathVariable String id) {
+    public ResponseEntity<?> deleteQuiz(@PathVariable String id, Authentication authentication) {
+        String authorName = quizService.getQuizAuthorName(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        if (!Objects.equals(authorName, authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         quizService.deleteById(id);
-        return new ResponseEntity<>("quiz deleted successfully", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    private QuizDTO convertToDTO(Quiz quiz) {
-        return new QuizDTO.Builder()
-                .setId(quiz.getId())
-                .setAuthor(userService.getName(quiz.getAuthorId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "author not found")))
-                .setDescription(quiz.getDescription())
-                .setPublic(quiz.getPublic())
-                .setQuestions(quiz.getQuestions())
-                .setTitle(quiz.getTitle())
-                .build();
+    @PostMapping("/{quizId}/{questionId}/updatestats")
+    public ResponseEntity<?> updateQuizStatistics(
+            @PathVariable String quizId,
+            @PathVariable String questionId,
+            @RequestParam(required = false) Integer wrong,
+            @RequestParam(required = false) Integer correct,
+            @RequestParam(required = false) Boolean needsReview,
+            Authentication authentication
+    ) {
+        String userId = userService.getId(authentication.getName()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        QuizStatistics quizStatistics = quizService.findQuizStatistics(userId, quizId).orElse(new QuizStatistics(userId, quizId, new HashMap<>()));
+        QuestionStatistics questionStatistics = Optional.ofNullable(quizStatistics.getQuestionsStatistics().get(questionId)).orElse(new QuestionStatistics());
+        if (wrong != null)
+            questionStatistics.newWrongAnswers(wrong);
+        if (correct != null)
+            questionStatistics.newCorrectAnswers(correct);
+        if (needsReview != null)
+            questionStatistics.setNeedsReview(needsReview);
+        quizStatistics.getQuestionsStatistics().put(questionId, questionStatistics);
+        quizService.saveOrUpdateQuizStatistics(quizStatistics);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    private Quiz convertFromDTO(QuizDTO quizDTO) {
-        return new Quiz.Builder()
-                .setId(quizDTO.getId())
-                .setAuthorId(userService.getId(quizDTO.getAuthor()).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "author not found")))
-                .setDescription(quizDTO.getDescription())
-                .setPublic(quizDTO.getPublic())
-                .setQuestions(quizDTO.getQuestions())
-                .setTitle(quizDTO.getTitle())
-                .build();
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
     @Autowired
@@ -82,7 +123,7 @@ public class QuizRestController {
     }
 
     @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
+    public void setDtoMapper(DtoMapper dtoMapper) {
+        this.dtoMapper = dtoMapper;
     }
 }
